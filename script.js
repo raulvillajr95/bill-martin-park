bd10064f4cbddc464fc19878569b0d05cb9df3b3
Raul <raulvillajr95@gmail.com>
2022-05-11 18:24:14 -0700

    Complete mobile home page

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const rentalCall = document.getElementById('rental-call');
+const rulesCall = document.getElementById('rules-call');
 
 rentalCall.addEventListener('click', function() {
+  window.open('tel:19093496900');
+})
+rulesCall.addEventListener('click', function() {
   window.open('tel:19093496900');
 })
